2a700c443af8b5f066ae9ba421021ff1001d63a3
Jason Roundtree <jasontree123@gmail.com>
2019-04-28 13:27:14 -0700

    Add logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import {
     Nav,
     NavItem,
     NavLink } from 'reactstrap';
-import utensils from '../images/utensils.svg'
+import utensils from '../images/utensils.svg';
 import './NavBar.css';
 
 export default class NavBar extends React.Component {
