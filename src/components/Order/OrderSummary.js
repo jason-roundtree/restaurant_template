253b26492346f3d3d815fe539a6b8bd59f2be995
Jason Roundtree <jasontree123@gmail.com>
2019-05-06 21:29:44 -0700

    Fix show/hide order summary onClick

--- src/components/Order/OrderSummary.js
+++ src/components/Order/OrderSummary.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react'
+import caretUp from '../../images/caret-up.svg'
+import caretDown from '../../images/caret-down.svg'
 
 export default class OrderSummary extends Component {
     state = {
         summaryOpen: false
     }
     toggleDrawer = () => {
-        console.log('toggleOrderSummaryDrawer')
+        // console.log('toggleOrderSummaryDrawer ')
         this.setState({
             summaryOpen: !this.state.summaryOpen
         }, () => {
-            // TODO: change this to a bootstrap/materialUI component. Seems like targeting by id may be causing issue with onClicking twice
+            // TODO: change this to a bootstrap/materialUI component?
+            const orderSummaryParagraph = document.getElementById('order-summary')
             this.state.summaryOpen 
-                ? document.getElementById('order-summary').style.height = '40px'
-                : document.getElementById('order-summary').style.height = '250px'
+                ? orderSummaryParagraph.style.height = '250px'                
+                : orderSummaryParagraph.style.height = '40px'
         })
     }
     render() {
+        const caretIcon = this.state.summaryOpen ? caretDown : caretUp
         return (
             <div id="order-summary">
                 {/* TODO: format phone number to include dashes */}
+                
                 <p onClick={this.toggleDrawer}>
-                    Order Summary:
+                    <img 
+                        src={caretIcon} 
+                        alt={caretIcon}
+                        width='20px'
+                        height='20px'
+                        style={{marginRight: '10px'}}
+                    />
+                    Order Summary
+                    <img 
+                        src={caretIcon} 
+                        alt={caretIcon}
+                        width='20px'
+                        height='20px'
+                        style={{marginLeft: '10px'}}
+                    />
                 </p>
             </div>
         )
